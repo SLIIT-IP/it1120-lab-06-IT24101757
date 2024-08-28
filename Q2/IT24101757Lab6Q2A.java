817110317118e35f4d38e0f299128ff055b01af1
IT24101757 <It24101757@my.sliit.lk>
2024-08-28 08:54:10 +0530

    Add IT24101757Lab6Q2A.java to Q2

---
+++ Q2/IT24101757Lab6Q2A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class IT24101757Lab6Q2A
+{
+	public static  void main(String[] args)
+	{
+		Scanner input= new Scanner(System.in);
+		int count;
+		
+		count = 1;
+		while(count <= 10)
+		{
+			System.out.print(count + " ");
+			count = count + 1;
+		}
+	}
+}
